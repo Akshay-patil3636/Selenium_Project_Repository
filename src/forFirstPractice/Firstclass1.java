679de778be10e05a3823fba26ee7a151a58e0a2b
Akshay-patil3636 <patilakshay9251@gmail.com>
2023-01-28 00:51:01 +0530

    one class added by akshay

---
+++ src/forFirstPractice/Firstclass1.java
@@ -0,0 +1,14 @@
+package forFirstPractice;
+
+public class Firstclass1 {
+public static void method1(String str1) {
+	System.out.println(str1);
+	
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Firstclass1.method1("AkshayPatil");
+		
+	}
+
+}

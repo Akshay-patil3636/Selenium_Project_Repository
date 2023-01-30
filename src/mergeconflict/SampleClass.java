754b67660ab56e00f68cb5faf34a1f282bfc035e
Akshay-patil3636 <patilakshay9251@gmail.com>
2023-01-31 00:57:15 +0530

    Changes made by Akshay for class2

--- src/mergeconflict/SampleClass.java
+++ src/mergeconflict/SampleClass.java
@@ -11,5 +11,5 @@ public class SampleClass {
 
 		System.out.println("This MEthod Created By Rohan");
 	}
-
+    
 }
---
+++ src/mergeconflict/SampleClass2.java
@@ -0,0 +1,16 @@
+package mergeconflict;
+
+public class SampleClass2 {
+
+	public static void main(String[] args) {
+		
+		SampleClass2 a=new SampleClass2();
+		a.sample2method();
+	
+	}
+public void sample2method()
+{
+	System.out.println("first changes made by Akshay");
+	
+}
+}

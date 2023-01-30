422250db29d9729c187b0eba4e69c503a70e1d0c
rnpass <rohannavale555@gmail.com>
2023-01-31 00:01:01 +0530

    Changes Made By Rohan Dated on 30 Jan 2023

---
+++ src/mergeconflict/SampleClass.java
@@ -0,0 +1,15 @@
+package mergeconflict;
+
+public class SampleClass {
+
+	public static void main(String[] args) {
+
+		methodCreatedByRohan();
+	}
+
+	public static void methodCreatedByRohan() {
+
+		System.out.println("This MEthod Created By Rohan");
+	}
+
+}

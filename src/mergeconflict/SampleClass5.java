113605c81bedc35e6b62202924244d37104b19d2
rnpass <rohannavale555@gmail.com>
2023-02-03 00:40:36 +0530

    Chages Made By Rohan For Class 5

---
+++ src/mergeconflict/SampleClass5.java
@@ -0,0 +1,11 @@
+package mergeconflict;
+
+public class SampleClass5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Class 5 created By Rohan");
+	}
+
+}
